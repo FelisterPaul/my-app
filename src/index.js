6b9ef725a1cb978cd4b0a8be84754b78009dc84c
Felister Paul <felisterpaul4@gmail.com>
2025-07-30 08:26:09 +0300

    add pink background

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Hello (props) {
-  return <h1>Hello World I'm a barbier Engineer, {props.name}!</h1>;
+function Hello(props) {
+  return (
+    <div style={{ backgroundColor: 'pink', padding: '20px' }}>
+      <h1>I'm a barbier Engineer, {props.name}!</h1>
+    </div>
+  );
 }
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Hello/>);
-
+root.render(<Hello name="Felister" />);
