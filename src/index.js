0adebc6044ef7ee78b2cc82900c6ad53b743ea42
Felister Paul <felisterpaul4@gmail.com>
2025-07-30 11:21:13 +0300

    added Bio data

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ function Hello(props) {
   return (
     <div style={{ backgroundColor: 'pink', padding: '20px', textAlign: 'center' }}>
       <h1>I'm a Barbier Engineer, {props.name}!</h1>
+      <h2>Welcome to my profile</h2>
       <img 
         src="https://via.placeholder.com/150" 
         alt="My portrait" 
-        style={{ borderRadius: '50%', marginBottom: '15px' }} 
+        style={{ borderRadius: '70%', marginBottom: '25px' }} 
       />
       <p>
         Hello! My name is {props.name}. I'm passionate about engineering and creativity. 
